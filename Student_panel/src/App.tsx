499e28fdba11528385277372dcf3a2dd7651a636
Sumedh-Raut <sumedhr9b4420@gmail.com>
2026-02-07 09:18:41 +0530

    S1

---
+++ Student_panel/src/App.tsx
@@ -0,0 +1,40 @@
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { AppLayout } from "./components/layout/AppLayout";
+import Index from "./pages/Index";
+import Profile from "./pages/Profile";
+import Networking from "./pages/Networking";
+import Jobs from "./pages/Jobs";
+import Login from "./pages/Login";
+import Events from "./pages/Events";
+import SettingsPage from "./pages/Settings";
+import NotFound from "./pages/NotFound";
+
+const queryClient = new QueryClient();
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <Toaster />
+      <Sonner />
+      <BrowserRouter>
+        <AppLayout>
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/networking" element={<Networking />} />
+            <Route path="/jobs" element={<Jobs />} />
+            <Route path="/events" element={<Events />} />
+            <Route path="/settings" element={<SettingsPage />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </AppLayout>
+      </BrowserRouter>
+    </TooltipProvider>
+  </QueryClientProvider>
+);
+
+export default App;
